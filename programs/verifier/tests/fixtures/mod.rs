b6436bb21216f6d7b2136f779d8f00db23a2f048
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2025-09-08 10:22:06 +0200

    add witness fixture

--- programs/verifier/tests/fixtures/mod.rs
+++ programs/verifier/tests/fixtures/mod.rs
@@ -3,14 +3,15 @@ pub mod config;
 pub mod constraint_coefficients;
 pub mod fri_commitment;
 pub mod fri_config;
+pub mod fri_decommitment;
 pub mod fri_unsent_commitment;
 pub mod interaction_elements;
 pub mod oods_values;
 pub mod public_input;
+pub mod queries;
 pub mod stark_commitment;
 pub mod stark_config;
 pub mod stark_domains;
+pub mod trace_witness;
 pub mod unsent_commitment;
 pub mod witness;
-
-pub mod queries;
