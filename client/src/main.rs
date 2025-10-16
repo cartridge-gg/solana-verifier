f145024d43f6d247767422de072be0161c3957b7
Michal <michalpalkowski3@wp.pl>
2025-10-16 16:38:20 +0200

    preparing for deployment

--- client/src/main.rs
+++ client/src/main.rs
@@ -18,17 +18,38 @@ enum Subcommands {
     RetriveFunds(Config),
 }
 
-#[tokio::main]
+fn main() {
+    let result = std::thread::Builder::new()
+        .stack_size(32 * 1024 * 1024)
+        .spawn(|| {
+            env_logger::Builder::from_default_env()
+                .filter_level(log::LevelFilter::Info)
+                .init();
+
+            let rt = tokio::runtime::Builder::new_multi_thread()
+                .thread_stack_size(32 * 1024 * 1024)
+                .enable_all()
+                .build()
+                .unwrap();
+
+            rt.block_on(async_main())
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+
+    if let Err(e) = result {
+        eprintln!("Error: {:?}", e);
+        std::process::exit(1);
+    }
+}
+
 #[allow(clippy::result_large_err)]
-async fn main() -> client::Result<()> {
-    env_logger::Builder::from_default_env()
-        .filter_level(log::LevelFilter::Info)
-        .filter_module("client", log::LevelFilter::Trace)
-        .init();
+async fn async_main() -> client::Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        Subcommands::Verify(config) => verify::verify(&config).await?,
         Subcommands::Deploy(config) => deploy::deploy(&config).await?,
+        Subcommands::Verify(config) => verify::verify(&config).await?,
         Subcommands::RetriveFunds(config) => retrive_funds::retrive_funds(&config).await?,
     }
     Ok(())
